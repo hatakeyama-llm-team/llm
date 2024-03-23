b0d29a8d2675f34ea95bfc9b747f2591d42ea96a
kanhatakeyama <none>
2024-03-23 17:04:08 +0900

    count tokens

--- codes/2_pretrain/count_tokens.py
+++ codes/2_pretrain/count_tokens.py
@@ -4,13 +4,13 @@ import os
 from tqdm import tqdm
 import yaml
 
-with open('sentence_piece_config.yaml', 'r') as file:
+with open('config.yaml', 'r') as file:
     conf= yaml.safe_load(file)
 print(conf)
 
 
 import sentencepiece as spm
-model_path=conf["output_dir"]+"/tokenizer.model"
+model_path=conf["input_tokenizer_file"]#+"/tokenizer.model"
 sp = spm.SentencePieceProcessor(model_file=model_path)
 
 
@@ -18,7 +18,7 @@ sp = spm.SentencePieceProcessor(model_file=model_path)
 total_tokens=0
 import json
 from tqdm import tqdm
-with open(conf["input"],"r") as f:
+with open(conf["input_jsonl"],"r") as f:
     for line in tqdm(f):
         text=json.loads(line)["text"]
         n_tokens=len(sp.encode(text, out_type=str))
