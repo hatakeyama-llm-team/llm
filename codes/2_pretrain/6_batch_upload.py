6eb3a7791fd18bc8efb7ab3fb32922d5e3b5f6fb
kanhatakeyama <none>
2024-05-22 11:04:51 +0900

    conv

--- codes/2_pretrain/6_batch_upload.py
+++ codes/2_pretrain/6_batch_upload.py
@@ -1,15 +1,17 @@
 # %%
 import glob
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 # %%
 model_dirs=glob.glob("../../models/hf/*")
-
-model_dir=model_dirs[0]
+#model_dirs=[model_dir for model_dir in model_dirs if model_dir.find("epoch")>=0]
 
 def upload_model(model_dir):
     model_name=model_dir.split("/")[-1]
-    model=AutoModelForCausalLM.from_pretrained(model_dir,device_map="auto")
+    model=AutoModelForCausalLM.from_pretrained(model_dir,device_map="auto",
+      torch_dtype=torch.bfloat16,
+    )
     tokenizer=AutoTokenizer.from_pretrained(model_dir)
 
     model.push_to_hub(model_name)
